aee29d5512703bcb750570b59653b581294230e0
Chang (Chris) Qian <59094765+QianChris@users.noreply.github.com>
2026-04-18 17:15:51 +0800

    VertexBufferLayout

--- Vulkitten/src/Vulkitten/Application.cpp
+++ Vulkitten/src/Vulkitten/Application.cpp
@@ -12,7 +12,30 @@ namespace Vulkitten
 {
     Application* Application::s_Instance = nullptr;
 
-#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case ShaderDataType::Float:   return GL_FLOAT;
+            case ShaderDataType::Float2:  return GL_FLOAT;
+            case ShaderDataType::Float3:  return GL_FLOAT;
+            case ShaderDataType::Float4:  return GL_FLOAT;
+            case ShaderDataType::Mat3:    return GL_FLOAT;
+            case ShaderDataType::Mat4:    return GL_FLOAT;
+            case ShaderDataType::Int:     return GL_INT;
+            case ShaderDataType::Int2:    return GL_INT;
+            case ShaderDataType::Int3:    return GL_INT;
+            case ShaderDataType::Int4:    return GL_INT;
+            case ShaderDataType::Uint:    return GL_UNSIGNED_INT;
+            case ShaderDataType::Uint2:   return GL_UNSIGNED_INT;
+            case ShaderDataType::Uint3:   return GL_UNSIGNED_INT;
+            case ShaderDataType::Uint4:   return GL_UNSIGNED_INT;
+            case ShaderDataType::Bool:    return GL_BOOL;
+        }
+
+        VKT_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
 
     Application::Application()
     {
@@ -20,7 +43,7 @@ namespace Vulkitten
         s_Instance = this;
 
         m_Window = std::unique_ptr<Window>(Window::Create());
-        m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+        m_Window->SetEventCallback(VKT_BIND_EVENT_FN(Application::OnEvent));
 
         m_ImGuiLayer = new ImGuiLayer();
         PushOverlay(m_ImGuiLayer);
@@ -29,28 +52,48 @@ namespace Vulkitten
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.0f,  0.5f, 0.0f
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,1.0f,
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,1.0f,
+             0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,1.0f,
         };
         m_VBO.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
         unsigned int indices[3] = { 0, 1, 2 };
         m_IBO.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        {
+            BufferLayout layout = {
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" },
+            };
+            m_VBO->SetLayout(layout);
+        }
 
+        uint32_t index = 0;
+        for (auto& element : m_VBO->GetLayout().GetElements())
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index,
+                element.GetComponentCount(),
+                ShaderDataTypeToOpenGLBaseType(element.Type),
+                element.Normalized ? GL_TRUE : GL_FALSE,
+                m_VBO->GetLayout().GetStride(),
+                (const void*)element.Offset);
+            index++;
+        }
+
+        
         std::string vertexSrc = R"(
             #version 330 core
 
             layout(location = 0) in vec3 a_Position;
-            out vec3 v_Position;
+            layout(location = 1) in vec4 a_Color;
+            out vec4 v_Color;
 
             void main()
             {
-                v_Position = a_Position;
+                v_Color = a_Color;
                 gl_Position = vec4(a_Position, 1.0);
             }
         )"; 
@@ -58,11 +101,11 @@ namespace Vulkitten
             #version 330 core
 
             layout(location = 0) out vec4 color;
-            in vec3 v_Position;
+            in vec4 v_Color;
 
             void main()
             {
-                color = vec4(v_Position * 0.5 + 0.5, 1.0);
+                color = v_Color;
             }
         )";
 
@@ -104,7 +147,7 @@ namespace Vulkitten
     void Application::OnEvent(Event &e)
     {
         EventDispatcher dispatcher(e);
-        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+        dispatcher.Dispatch<WindowCloseEvent>(VKT_BIND_EVENT_FN(Application::OnWindowClose));
 
         for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
         {
